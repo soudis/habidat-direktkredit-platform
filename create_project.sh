a6473a4dca967f3a8e0da7d61b8e94275cc7693d
Florian Humer <hf@muehlboeck.com>
2020-05-27 19:52:21 +0200

    add certbot code

--- create_project.sh
+++ create_project.sh
@@ -16,8 +16,33 @@ python3 scripts/generate_project_compose.py $1 $2
 echo "Creating docker container..."
 docker-compose -p $1 -f projects/$1/docker-compose.yml up -d 
 
+echo "Creating let's encrypt certificates..."
+case "$LETSENCRYPT_EMAIL" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $LETSENCRYPT_EMAIL" ;;
+esac
+
+domain_args="-d $3"
+docker-compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $email_arg \
+    $domain_args \
+    --agree-tos" certbot
+
+if [[ $# -gt 3 ]]; then
+	for domain in "${@:4}"; do
+	  	domain_args="-d $domain"
+		docker-compose run --rm --entrypoint "\
+		  certbot certonly --webroot -w /var/www/certbot \
+		    $email_arg \
+		    $domain_args \
+		    --agree-tos" certbot	  
+	done
+fi
+
+sleep 30
+
 echo "Add project to nginx..."
 docker-compose exec nginx python3 scripts/add_project.py $1 dk_$1_web $3 ${@:4}
-sleep 30
 docker-compose exec nginx python3 scripts/generate_config.py
 
