3350b56ba88ca84e795f4da6f753c13daa6bb5d5
root <root@v2202005122260118727.supersrv.de>
2020-06-09 12:44:05 +0200

    test

--- logs.sh
+++ logs.sh
@@ -10,14 +10,14 @@ usage(){
 
 LOG_PARAMETERS="--tail=500 -t"
 
-[[ $# -lt 2 ]] && usage
-[[ $# -gt 2 ]] && LOG_PARAMETERS=${@:2}
+[[ $# -lt 1 ]] && usage
+[[ $# -gt 1 ]] && LOG_PARAMETERS=${@:2}
 
 if [[ $1 == "proxy" ]]
 then
 	docker-compose logs $LOG_PARAMETERS nginx
 else
 	cd projects/$1
-	docker-compose logs $LOG_PARAMETERS nginx
+	docker-compose logs $LOG_PARAMETERS web
 	cd ../..
 fi
--- update_projects.sh
+++ update_projects.sh
@@ -8,19 +8,21 @@ usage(){
 	exit 1
 }
 
-[[ $# -lt 2 ]] && usage
-[[ $# -gt 2 ]] && usage
+[[ $# -lt 1 ]] && usage
+[[ $# -gt 1 ]] && usage
 
 if [[ $1 == "all" ]]
 then
 	echo "Updating all projects..."
-	for project in projects/*/ ; do
+	cd projects
+	for project in * ; do
 		echo "Updating project $project..."    
-		cd projects/$project
+		cd $project
 		docker-compose pull 
 		docker-compose up -d
-		cd ../../
+		cd ..
 	done
+	cd ..
 elif [[ $1 == "proxy" ]]
 then
 	echo "Updating proxy containers..."
