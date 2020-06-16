1a2e3831a346b58fbd5f7c192c882836f351fce3
Florian Humer <hf@muehlboeck.com>
2020-06-16 12:29:32 +0200

    add update project script

---
+++ update_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+source settings.env
+
+usage(){
+	echo "Usage: $0 <Project ID> <URL> [<URL> ...]"
+	exit 1
+}
+
+echo "Update project..."
+docker-compose exec nginx python3 scripts/update_project.py $1 dk_$1_web ${@:2}
+docker-compose exec nginx python3 scripts/generate_config.py
+
