3d4a4669398416d8de081e0ccff96321fd24f00b
Florian Humer <hf@muehlboeck.com>
2020-06-21 17:13:26 +0200

    bugfixes

--- import_db.sh
+++ import_db.sh
@@ -12,8 +12,10 @@ usage(){
 [[ $# -lt 4 ]] && usage
 
 echo "Deleting old data..."
-docker exec $1 mysqldump -u root -p$3 $2 --add-drop-table --no-data | grep 'DROP_TABLE' > tmp_deletetables.sql
-cat tmp_deletetables.sql | docker exec -i $1 mysql -u root -p$3 $2 mysql
-echo "import new data from $4..."
+echo "SET FOREIGN_KEY_CHECKS = 0;" > tmp_deletetables.sql
+docker exec $1 mysqldump -u root -p$3 $2 --add-drop-table --no-data | grep 'DROP_TABLE' >> tmp_deletetables.sql
+echo "SET FOREIGN_KEY_CHECKS = 1;" >> tmp_deletetables.sql
+cat tmp_deletetables.sql | docker exec -i $1 mysql -u root -p$3 $2
+echo "Import new data from $4..."
 cat $4 | docker exec -i $1 mysql -u root -p$3 $2 mysql
 
