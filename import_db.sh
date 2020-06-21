1b0257f6eac13ee4efb6d9afe7f3269f60c17399
Florian Humer <hf@muehlboeck.com>
2020-06-21 17:02:53 +0200

    bugfixes

---
+++ import_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+source settings.env
+
+usage(){
+	echo "Usage: $0 <container> <database> <root password> <database dump file>"
+	exit 1
+}
+
+echo "Deleting old data..."
+docker exec $1 mysql -u root -p$3 $2 mysqldump --add-drop-table --no-data | grep 'DROP_TABLE' > tmp_deletetables.sql
+cat tmp_deletetables.sql | docker exec -i $1 mysql -u root -p$3 $2 mysql
+echo "import new data from $4..."
+cat $4 | docker exec -i $1 mysql -u root -p$3 $2 mysql
+
