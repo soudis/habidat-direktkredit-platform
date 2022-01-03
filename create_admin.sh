58d4f11819137e620c5659be96c3254e1b4e13c1
Florian Humer <florian.humer@gmail.com>
2022-01-03 19:41:33 +0100

    add create_admin.sh

--- create_admin.sh
+++ create_admin.sh
@@ -12,6 +12,6 @@ usage(){
 
 cd projects/$1
 
-echo "insert into admin (logon_id, email, passwordHashed, loginCount) values ('$2', '$2', 'nohash', 0);" | docker-compose -p $HABIDAT_DK_CONTAINER_PREFIX-$1 exec mysql -u root -"p$3"
+echo "insert into admin (logon_id, email, passwordHashed, loginCount) values ('$2', '$2', 'nohash', 0);" | docker-compose -p $HABIDAT_DK_CONTAINER_PREFIX-$1 exec -T db mysql -u root -"p$3"
 
 cd ../..
