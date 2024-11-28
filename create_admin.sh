92b25f0b73fa1e3f67ca5c7736462500d2a742fe
Florian Humer <florian.humer@gmail.com>
2024-11-28 10:58:51 +0100

    feat: simplify create admin

--- create_admin.sh
+++ create_admin.sh
@@ -4,14 +4,16 @@ set -e
 source settings.env
 
 usage(){
-	echo "Usage: $0 <Project ID> <Admin E-mail Address> <DB root password>"
+	echo "Usage: $0 <Project ID> 
 	exit 1
 }
 
-[[ $# -lt 3 ]] && usage
+[[ $# -lt 2 ]] && usage
 
 cd projects/$1
 
-echo "insert into admin (logon_id, email, passwordHashed, loginCount, createdAt, updatedAt) values ('$2', '$2', 'nohash', 0, NOW(), NOW());" | docker compose -p $HABIDAT_DK_CONTAINER_PREFIX-$1 exec -T db mariadb -u root -"p$3" "dk_$1_db"
+STMT="insert into admin (logon_id, email, passwordHashed, loginCount, createdAt, updatedAt) values ('$2', '$2', 'nohash', 0, NOW(), NOW());"
+
+./execute_sql.sh $1 "$STMT"
 
 cd ../..
